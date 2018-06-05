1d577448f47a04244a235e787d65e9eb0f6db1ad
sharzephoue <sharzephoue@gmail.com>
2018-06-05 14:03:57 +0200

    final

--- contract/src/main/java/model/IModel.java
+++ contract/src/main/java/model/IModel.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.sql.SQLException;
-import java.util.List;
 
 /**
  * <h1>The Interface IModel.</h1>
@@ -11,34 +10,19 @@ import java.util.List;
  */
 public interface IModel {
 
-    /**
-     * Gets the example by id.
-     *
-     * @param id
-     *            the id
-     * @return the example by id
-     * @throws SQLException
-     *             the SQL exception
-     */
-    Example getExampleById(int id) throws SQLException;
+	/**
+	 * Gets the level by id.
+	 *
+	 * @param id
+	 *            the id
+	 * @return the example by id
+	 * @throws SQLException
+	 *             the SQL exception
+	 */
+	void getLevelById(int id) throws SQLException;
 
-    /**
-     * Gets the example by name.
-     *
-     * @param name
-     *            the name
-     * @return the example by name
-     * @throws SQLException
-     *             the SQL exception
-     */
-    Example getExampleByName(String name) throws SQLException;
+	IMobile getLorann();
 
-    /**
-     * Gets the all examples.
-     *
-     * @return the all examples
-     * @throws SQLException
-     *             the SQL exception
-     */
-    List<Example> getAllExamples() throws SQLException;
+	//ArrayList<Golem> getMonster();
+
 }
